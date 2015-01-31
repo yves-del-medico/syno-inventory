9c8a1b20d990ad7d0611f74fb4887694849eb7d1
Yves Del-Medico <yves.del.medico@gmail.com>
2015-01-31 19:31:44 +0100

    Added config file, extract ID3

--- main.js
+++ main.js
@@ -1,50 +1,178 @@
 var Walker = require('walker');
+var fs = require('fs');
+var path = require('path');
+var Q = require('q');
+var id3 = require('id3js');
 
-var prefix = '/volume1/homes/admin/';
-var dir = 'syno-inventory-test';
+var list = [];
 
-var prefixLen = prefix.length;
+function loadConfig() {
+  var configname = path.join(process.env.HOME, '.syno-inventory.json');
+  var config = fs.readFileSync(configname, {});
+  config = JSON.parse(config);
+  var dirs = {};
 
-function removePrefix(name) {
-    return name.substr(prefixLen);
-}
+  config.exclude = config.exclude || [];
 
-Walker(prefix + dir)
-  .filterDir(function(dir, stat) {
-    if (dir === '/etc/pam.d') {
-      console.warn('Skipping /etc/pam.d and children')
-      return false
+  config.directories.forEach(function(entry) {
+    var name = entry.dir;
+    if (entry.dir.substr(0, 1) === '~') {
+      entry.dir = entry.dir.replace(/~/, process.env.HOME);
     }
-    return true
-  })
-  .on('entry', function(entry, stat) {
-    // console.log('Got entry: ' + entry)
-  })
-  .on('dir', function(dir, stat) {
-    console.log('Got directory: ' + removePrefix(dir));
-  })
-  .on('file', function(file, stat) {
-    console.log('Got file: ' + removePrefix(file));
-  })
-  .on('symlink', function(symlink, stat) {
-    console.log('Got symlink: ' + symlink);
-  })
-  .on('blockDevice', function(blockDevice, stat) {
-    console.log('Got blockDevice: ' + blockDevice)
-  })
-  .on('fifo', function(fifo, stat) {
-    console.log('Got fifo: ' + fifo)
-  })
-  .on('socket', function(socket, stat) {
-    console.log('Got socket: ' + socket)
-  })
-  .on('characterDevice', function(characterDevice, stat) {
-    console.log('Got characterDevice: ' + characterDevice)
-  })
-  .on('error', function(er, entry, stat) {
-    console.log('Got error ' + er + ' on entry ' + entry)
-  })
-  .on('end', function() {
-    console.log('All files traversed.')
+    entry.dir = path.normalize(entry.dir);
+    entry.dir = entry.dir.replace(/\/$/, '');
+
+    if (dirs[entry.dir]) {
+      console.log("ERROR: duplicate entry: ", name);
+      process.exit();
+    } else {
+      dirs[entry.dir] = entry;
+    }
+
+    entry.exclude = entry.exclude || [];
+    entry.exclude = entry.exclude.concat(config.exclude);
   });
 
+  return config;
+}
+
+function walk(config) {
+
+  console.log("Walk: ", config);
+  var deferred = Q.defer();
+
+  Walker(path.join(config.dir))
+    .filterDir(function(dir, stat) {
+      if (config.exclude.indexOf(path.basename(dir)) >= 0) {
+        console.warn('Skipping dir', dir);
+        return false;
+      }
+
+      return true;
+    })
+    .on('entry', function(entry, stat) {
+      // console.log('Got entry: ' + entry)
+    })
+    .on('dir', function(dir, stat) {
+      console.log('Got directory: ' + dir);
+    })
+    .on('file', function(file, stat) {
+      var name = path.relative(config.dir, file);
+      if (excludeFile(file, config)) {
+        console.log('Skipping file: ' + name);
+      } else {
+        console.log('Got file: ' + name);
+        list.push({
+          dir: config.dir,
+          name: name,
+          // size: stat.size,
+          // ctime: stat.ctime,
+          // mtime: stat.mtime
+        });
+      }
+    })
+    .on('symlink', function(symlink, stat) {
+      console.log('Got symlink: ' + symlink);
+    })
+    .on('blockDevice', function(blockDevice, stat) {
+      console.log('Got blockDevice: ' + blockDevice);
+    })
+    .on('fifo', function(fifo, stat) {
+      console.log('Got fifo: ' + fifo);
+    })
+    .on('socket', function(socket, stat) {
+      console.log('Got socket: ' + socket);
+    })
+    .on('characterDevice', function(characterDevice, stat) {
+      console.log('Got characterDevice: ' + characterDevice);
+    })
+    .on('error', function(er, entry, stat) {
+      console.log('Got error ' + er + ' on entry ' + entry);
+      deferred.reject('Got error ' + er + ' on entry ' + entry);
+    })
+    .on('end', function() {
+      console.log('All files traversed.', config.dir);
+      deferred.resolve();
+    });
+
+    return deferred.promise;
+}
+
+function extractId3() {
+    console.log("Extract ID3 info");
+    var result = Q(1);
+
+    list.forEach(function(entry) {
+      if (/.mp3$/.test(entry.name)) {
+        result = result.then(function() {
+
+          console.log('ID3:', entry.name);
+          var deferred = Q.defer();
+
+          id3({ file: path.join(entry.dir, entry.name), type: id3.OPEN_LOCAL }, function(err, tags) {
+            if (err) {
+              console.log('ID3 error', err);
+              deferred.reject(err);
+              return;
+            }
+
+            // tags now contains your ID3 tags
+            addId3Info(entry, tags);
+            // console.log('ID3', tags);
+            deferred.resolve();
+          });
+
+          return deferred.promise;
+        });
+      }
+    });
+    return result;
+}
+
+function addId3Info(entry, tags) {
+  var id3 = {};
+  var items = ['title', 'artist', 'album', 'year'];
+  items.forEach(function(key) {
+    if (tags[key]) {
+      id3[key] = tags[key].replace(/\u0000/g, '');
+    }
+  });
+  entry.id3 = id3;
+}
+
+function excludeFile(file, config) {
+  var found = false;
+  config.exclude.forEach(function(pattern) {
+    // console.log('Checking', file, 'with pattern >>' + pattern + '<<');
+    var patt = new RegExp(pattern);
+    if (patt.test(file)) {
+      found = true;
+    }
+  });
+  return found;
+}
+
+function main() {
+  var config = loadConfig();
+  var result = Q(1);
+
+  // Queue all the directories to process
+  config.directories.forEach(function(entry) {
+    if (entry.enabled) {
+      result = result.then(function() {
+        return walk(entry);
+      });
+    }
+  });
+
+  result = result.then(function() {
+    return extractId3();
+  });
+
+  // Display the result at the end
+  result.then(function() {
+    console.log('List:', list);
+  });
+}
+
+main();
